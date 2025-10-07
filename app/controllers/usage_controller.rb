2d52a2ec9e0cabf7c97f948f994ec50d83562574
shan <2302531309@qq.com>
2025-10-07 10:36:22 +0800

    Revert "Update usage_controller.rb"
    This reverts commit d3f55644ad7dbde801b299e3c413aba8807cc971.

--- app/controllers/usage_controller.rb
+++ app/controllers/usage_controller.rb
@@ -1,10 +1,8 @@
 class UsageController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :check_if_maintenance_mode
   around_action :check_credits
-  def current_user
-    User.first
-  end
+
   include CreditsCounter
   include DistributedLock
 
