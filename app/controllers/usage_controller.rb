1929539bbb1a0e99ae4f5286adbd1ddd075b9300
shan <2302531309@qq.com>
2025-10-07 11:34:00 +0800

    Revert "test2"
    This reverts commit fe3b10e083689eb1025ecdd3456dc0e070901040.

--- app/controllers/usage_controller.rb
+++ app/controllers/usage_controller.rb
@@ -1,10 +1,8 @@
 class UsageController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :check_if_maintenance_mode
   around_action :check_credits
-  def current_user
-    User.first
-  end
+
   include CreditsCounter
   include DistributedLock
 
@@ -31,7 +29,6 @@ class UsageController < ApplicationController
     @ai_call = nil
 
     with_redis_lock(current_user.id) do
-      sleep 30
       if credits_enough?(current_cost_credits)
         conversation = current_user.conversations.create
         @ai_call = conversation.ai_calls.create(
